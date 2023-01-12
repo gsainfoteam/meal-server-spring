d2b4ba1b6102026f0563f262b8818739c3485f64
김산 <kimsan@gimsan-ui-MacBookAir.local>
2023-01-12 17:17:59 +0900

    project setting 2

---
+++ src/main/java/com/example/helper/controller/MealController.java
@@ -0,0 +1,78 @@
+package com.example.helper.controller;
+import com.example.helper.dto.MealWrapper;
+import com.example.helper.entity.Meal;
+import com.example.helper.repository.MealRepository;
+import com.example.helper.service.MealService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@RequestMapping(path = "/meals", produces = "application/json;charset=UTF-8")
+@Slf4j
+public class MealController {
+
+    @Autowired
+    private MealService mealService;
+
+    @GetMapping("/all")
+    public String hello() {
+        return "Hello HELPERs";
+    }
+
+    @PostMapping("/create")
+    public @ResponseBody String createMeal(MealWrapper mealWrapper) {
+
+        Meal meal = mealWrapper.getMeal();
+
+//        Meal saved = mealService.save(meal);
+
+        return "saved"; // Responsebody 넣어서 보내는거 해보세요
+    }
+
+    @PostMapping("/kor")
+    public @ResponseBody String readKorMeal() {
+        // input : utc
+        // output : 카톡 서버맞게 한국어식단 JSON으로
+//        const responseBody = {
+//                version: "2.0",
+//                template: {
+//            outputs: [
+//            {
+//                simpleText: {
+//                    text: rows[0].menu
+//                }
+//            }
+//          ]
+//        }
+//      };
+//        res.status(200).send(responseBody);
+        return "saved"; // Responsebody 넣어서 보내는거 해보세요
+    }
+
+    @PostMapping("/eng")
+    public @ResponseBody String readEngMeal() {
+        // input : utc
+        // output : 카톡 서버맞게 영어식단 JSON으로
+
+        return "saved"; // Responsebody 넣어서 보내는거 해보세요
+    }
+
+    @PostMapping("/speckor")
+    public @ResponseBody String readSpecKorMeal() {
+        // input : 날짜요일내일 + 아점저 + 1/2학
+        // output : 해당 식단 찾아 카톡 서버맞게 JSON으로
+
+        return "saved"; // Responsebody 넣어서 보내는거 해보세요
+    }
+
+    @PostMapping("/speceng")
+    public @ResponseBody String readSpecEngMeal() {
+        // input : 날짜요일내일 + 아점저 + 1/2학
+        // output : 해당 식단 찾아 카톡 서버맞게 JSON으로
+
+        return "saved"; // Responsebody 넣어서 보내는거 해보세요
+    }
+}
--- src/main/java/com/example/helper/service/MealService.java
+++ src/main/java/com/example/helper/service/MealService.java
@@ -3,35 +3,38 @@ package com.example.helper.service;
 import com.example.helper.entity.Meal;
 import com.example.helper.repository.MealRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
-public class MealService (Meal meal) {
+@Service
+public class MealService {
+
     @Autowired
     private MealRepository mealRepository;
-
-    public Meal save(Meal mael) {
-        Meal n = new Meal();
-
-        n.setTitle(meal.getTitle());
-        n.setStart(reqMeal.getStart());
-        n.setEnd(reqMeal.getEnd());
-        n.setUser(reqMeal.getUser());
-
-        // 중복 모르겠고 일단 다 넣자.
-//        if (
-//                !timetableService.validateDay(reqTime.getDay())
-//                        || !timetableService.validateTime(reqTime.getStart(), reqTime.getEnd())
-//                        || timetableService.isDuplicateNew(n, newBuildingTimetableRepository.findByDay(reqTime.getDay()))
-//        ) {
-//            Throwable ex = new Throwable();
-//            throw new ResponseStatusException(
-//                    HttpStatus.NOT_ACCEPTABLE, "Invalid day, time", ex);
-//        }
-//        mealRepository.save(n);
-    }
-
-    public Meal findByMeal(Meal meal) {
-        Meal result = new Meal();
-        Integer target = mealRepository.findByDateTitlealfajflkfdj()
-
-    }
+//
+//    public Meal save(Meal mael) {
+//        Meal n = new Meal();
+//
+//        n.setTitle(meal.getTitle());
+//        n.setStart(reqMeal.getStart());
+//        n.setEnd(reqMeal.getEnd());
+//        n.setUser(reqMeal.getUser());
+//
+//        // 중복 모르겠고 일단 다 넣자.
+////        if (
+////                !timetableService.validateDay(reqTime.getDay())
+////                        || !timetableService.validateTime(reqTime.getStart(), reqTime.getEnd())
+////                        || timetableService.isDuplicateNew(n, newBuildingTimetableRepository.findByDay(reqTime.getDay()))
+////        ) {
+////            Throwable ex = new Throwable();
+////            throw new ResponseStatusException(
+////                    HttpStatus.NOT_ACCEPTABLE, "Invalid day, time", ex);
+////        }
+////        mealRepository.save(n);
+//    }
+//
+//    public Meal findByMeal(Meal meal) {
+//        Meal result = new Meal();
+//        Integer target = mealRepository.findByDateTitlealfajflkfdj()
+//
+//    }
 }
