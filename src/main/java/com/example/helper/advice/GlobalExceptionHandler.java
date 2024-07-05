dc7e96ad2e387afb37ccdf5e68633a581db01539
2paperstar <lee@paperst.ar>
2024-07-05 18:11:23 +0900

    fix: RestControllerAdvice status code

--- src/main/java/com/example/helper/advice/GlobalExceptionHandler.java
+++ src/main/java/com/example/helper/advice/GlobalExceptionHandler.java
@@ -1,14 +1,15 @@
 package com.example.helper.advice;
 
 import com.example.helper.dto.ErrorDto;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.server.ResponseStatusException;
 
 @RestControllerAdvice
-public class NotFoundAdvice {
+public class GlobalExceptionHandler {
     @ExceptionHandler(ResponseStatusException.class)
-    public ErrorDto handle(ResponseStatusException e) {
-        return new ErrorDto(e.getReason());
+    public ResponseEntity<?> handle(ResponseStatusException e) {
+        return ResponseEntity.status(e.getStatusCode()).body(new ErrorDto(e.getReason()));
     }
 }
